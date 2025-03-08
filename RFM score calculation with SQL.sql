dcdaecbe04ca3d35c84d8fcadff7715d0cd60ce8
Bahareh Fallahi <39549087+bhx98@users.noreply.github.com>
2025-03-08 11:30:16 +0330

    Update RFM score calculation with SQL.sql
    FIX Errors

--- RFM score calculation with SQL.sql
+++ RFM score calculation with SQL.sql
@@ -1,4 +1,4 @@
-select ContactName, rfm_recency, rfm_frequency, rfm_monetary, round((rfm_monetary + rfm_recency + rfm_frequency)/3) as rfm_score 
+select ContactName, rfm_recency, rfm_frequency, rfm_monetary, round((rfm_monetary + rfm_recency + rfm_frequency)/3,2) as rfm_score 
 from (
 select ContactName,
 	ntile(5) over (order by last_order_date) as rfm_recency,
@@ -12,7 +12,7 @@ select cus.ContactName,
        count(*) as count_order,
        avg(ordd.UnitPrice) as avg_amount
 from orders ord
-left join `order details` ordd
+left join [order details] ordd
 on ordd.OrderID = ord.OrderID
 left join customers cus 
 on cus.CustomerID = ord.CustomerID 
